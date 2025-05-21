3d51c51975c192f494ccd055d8ffc58521b880e3
lizegira <lizegira@bancolombia.com.co>
2025-05-21 16:17:36 -0500

    styles

--- hostmaster-app/src/components/Rooms/RoomTable.tsx
+++ hostmaster-app/src/components/Rooms/RoomTable.tsx
@@ -122,7 +122,7 @@ const RoomTable: React.FC<RoomTableProps> = ({
                   />
                 </td>
                 <td>
-                  <div className="d-flex flex-column flex-md-row gap-2 ">
+                  <div>
                     <Button
                       variant="warning"
                       size="sm"
